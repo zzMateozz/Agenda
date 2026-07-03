654b12ab537136a5e17de08fee57ba57eec691ee
Mateo <mateolopez@unicesar.edu.co>
2026-07-03 14:06:35 -0500

    feat: Creacion del mdoelo de contacto

---
+++ src/agenda/models.py
@@ -0,0 +1,8 @@
+class Contacto:
+    def __init__(self, nombre, telefono, correo = None):
+        self.nombre = nombre
+        self.telefono = telefono
+        self.correo = correo
+
+    def __str__(self):
+        return f"Nombre: {self.nombre}, Teléfono: {self.telefono}, Correo: {self.correo}"
